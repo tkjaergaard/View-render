4ac8633366fa5e65b8b4aab157185a460d617f8f
t_kjaergaard <tkj@tkjaergaard.dk>
2013-05-05 11:53:30 +0200

    First commit

---
+++ View.php
@@ -0,0 +1,18 @@
+<?php namespace Devdk;
+
+class View {
+
+    public static function make( $view, $data=FALSE )
+    {
+        if( !file_exists($view) )
+                throw new \Exception("View file not found: {$view}", 404);
+
+        if( $data && is_array($data) )
+                extract($data,EXTR_SKIP);
+
+        ob_start();
+                include $view;
+        return ob_get_clean();
+    }
+
+}
